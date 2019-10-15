a0abdc2bad19b7c249164268b340769886899ea2
valeria campilongo <cvaleriac@hotmail.it>
2019-10-15 17:54:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
- 
+
 const store = createStore(shoppingListItemReducer);
- 
+
 // code change - added Provider to wrap around App
 ReactDOM.render(
   <Provider store={store}>
